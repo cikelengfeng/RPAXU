0b970ddf875b1f9e5202e8b8a9d7337c3c2c5867
muyexi <muyexi@gmail.com>
2015-07-02 17:56:23 +0800

    replace defaults with plistbuddy

--- refreshPluginsAfterXcodeUpgrading.sh
+++ refreshPluginsAfterXcodeUpgrading.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
+#PlistBuddy的位置
+PLISTBUDDY="/usr/libexec/PlistBuddy"
+
 #获取当前版本Xcode的DVTPlugInCompatibilityUUID
 UUID=$(defaults read /Applications/Xcode.app/Contents/Info DVTPlugInCompatibilityUUID)
 echo Xcode DVTPlugInCompatibilityUUID is $UUID
+
 #遍历每一个Xcode插件，将UUID写入插件的兼容列表中
 for MyPlugin in ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/*
 do
-	defaults write "$MyPlugin"/Contents/Info DVTPlugInCompatibilityUUIDs -array-add $UUID
-	echo write DVTPlugInCompatibilityUUID to $MyPlugin succeeded!
+	$PLISTBUDDY -c "Add :DVTPlugInCompatibilityUUIDs: string $UUID" "$MyPlugin"/Contents/Info.plist
+	echo write DVTPlugInCompatibilityUUID to $MyPlugin succeed!
 done
